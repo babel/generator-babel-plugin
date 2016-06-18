988286b7b7b215986755205aee768f9176126235
HE Shi-Jun <hax@heshijun.net>
2016-06-19 05:02:19 +0800

    Normally we do not need import plugin

--- generators/app/templates/test/index.js
+++ generators/app/templates/test/index.js
@@ -2,7 +2,6 @@ import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
 import { transformFileSync } from 'babel-core';
-import plugin from '../src';
 
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
