4229b2786cc7f608472c1cb44b61fbada6fbe0f7
HE Shi-Jun <hax@heshijun.net>
2016-06-21 12:51:09 +0800

    prefer native trim() method (#16)
    Node 0.10 already have native `trim()`

--- generators/app/templates/test/index.js
+++ generators/app/templates/test/index.js
@@ -3,10 +3,6 @@ import fs from 'fs';
 import assert from 'assert';
 import { transformFileSync } from 'babel-core';
 
-function trim(str) {
-  return str.replace(/^\s+|\s+$/, '');
-}
-
 describe('<%= description %>', () => {
   const fixturesDir = path.join(__dirname, 'fixtures');
   fs.readdirSync(fixturesDir).map((caseName) => {
@@ -19,7 +15,7 @@ describe('<%= description %>', () => {
           path.join(fixtureDir, 'expected.js')
       ).toString();
 
-      assert.equal(trim(actual), trim(expected));
+      assert.equal(actual.trim(), expected.trim());
     });
   });
 });
